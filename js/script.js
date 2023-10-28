be122813e914ad1e30817bca76b9ce692c1b7517
n0m3k <christian.hoefler89@gmail.com>
2023-10-28 17:17:24 +0200

    update

--- js/script.js
+++ js/script.js
@@ -6,9 +6,21 @@ $(document).ready(function() {
     $('.nav-link').each(function() {
         let linkPath = $(this).attr('href');
 
+        $('.nav-item').removeClass('active');
+
         // Check if the link's href matches the current page path
         if (currentPath === linkPath) {
             $(this).closest('.nav-item').addClass('active');
         }
+    });
+});
+
+$(document).ready(function() {
+    $('.nav-link').click(function() {
+        // Remove the active class from all navigation items
+        $('.nav-item').removeClass('active');
+
+        // Add the active class to the parent li of the clicked link
+        $(this).parent().addClass('active');
     });
 });

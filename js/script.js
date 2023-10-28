8d5814d0f5a43251a81c1f65dbede62bffa81683
n0m3k <christian.hoefler89@gmail.com>
2023-10-28 18:08:28 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
-    // Get the current page path
+
     let currentPath = window.location.pathname;
+
     console.log(currentPath)
+
+    $('.navbar .nav-link').each(function() {
+        let linkPath = $(this).attr('href');
+
+        if (currentPath === linkPath) {
+            $(this).closest('.nav-item').addClass('active');
+            console.log("active");
+        } else {
+            $(this).closest('.nav-item').removeClass('active');
+            console.log("remove");
+        }
+    });
 });

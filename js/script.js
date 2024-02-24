99207b5aa98d42b2734b3f53534b2327185b7f6e
n0m3k <christian.hoefler89@gmail.com>
2024-02-24 17:23:31 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,23 +1,4 @@
 
-function preventCaching() {
-    let elements = document.querySelectorAll('link[rel="stylesheet"], script[src], img[src]');
-
-    elements.forEach(function(element) {
-        let url = element.getAttribute('href') || element.getAttribute('src');
-        if (url.indexOf('?') === -1) {
-            url += '?_=' + new Date().getTime(); // Append a unique timestamp as query parameter
-        } else {
-            url += '&_=' + new Date().getTime(); // Append a unique timestamp as query parameter
-        }
-        if (element.tagName.toLowerCase() === 'link') {
-            element.setAttribute('href', url);
-        } else {
-            element.setAttribute('src', url);
-        }
-    });
-}
-
-
 function activeMenu() {
     let currentPath = window.location.pathname;
 
@@ -34,5 +15,4 @@ function activeMenu() {
 
 $(document).ready(function() {
     activeMenu();
-    preventCaching();
 });
